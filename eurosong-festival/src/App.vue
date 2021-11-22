1b14c77a743cc4c672d0623bc17bc9029d293944
AyoubVD <ayoub.disneyti@gmail.com>
2021-11-22 10:50:59 +0100

    upd

--- eurosong-festival/src/App.vue
+++ eurosong-festival/src/App.vue
@@ -1,5 +1,11 @@
 <template>
   <div id="app">
+    <nav class="c-nav">
+      <ul class="c-nav-list">
+        <li class="c-nav-list-item">Home</li>
+        <li class="c-nav-list-item">Contact</li>
+      </ul>
+    </nav>
     <img alt="Vue logo" src="./assets/logo.png">
     <h1>Hello not online yet</h1>
     <h2>NVM, it's online</h2>
